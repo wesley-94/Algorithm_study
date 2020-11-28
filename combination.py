f45156165b5cc4e9b68826a4f2004b47f184b8b8
wesley-94 <rladhlthf123@naver.com>
2020-11-28 16:18:55 +0900

    ABOUT INNER SEQUENCE TYPES 2

---
+++ combination.py
@@ -0,0 +1,13 @@
+def combinations(s):
+    if len(s) < 2:
+        return s
+    res = []
+    for i, c in enumerate(s):
+        res.append(c) # 추가된 부분
+        for j in combinations(s[:i] + s[i+1:]):
+            res.append(c + j)
+    return res
+
+if __name__ == "__main__":
+    result = combinations("abc")
+    print(result)
