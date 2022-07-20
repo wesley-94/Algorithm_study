fdf6a68de0165345407e5fd2cb75bb5374db2ef1
wesley <rladhlthf123@naver.com>
2022-07-20 10:34:48 +0900

    Add 2022-07-20 files

---
+++ ThisIsCodingTest/모험가 길드_1.py
@@ -0,0 +1,17 @@
+# N 값 입력 받기 
+N = int(input())
+
+# Data List
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0 # 총 그룹의 수
+count = 0 # 현재 그룹에 포함된 모험가의 수
+
+for i in data: # 공포도를 낮은 것부터 하나씩 확인하며
+    count += 1 # 현재 그룹에 해당 모험가를 포함 시키기
+    if count >= i: # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+        result += 1 # 총 그룹의 수 증가 시키기
+        count = 0 # 현재 그룹에 포함된 모험가의 수 초기화
+        
+print(result) # 총 그룹의 수 출력
